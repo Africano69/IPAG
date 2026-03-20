315e5c7e24cce9bf92316a39192cc4ddb7b90eed
Africano <140811798+Africano69@users.noreply.github.com>
2026-03-20 10:06:48 +0100

    Delete sistema_escolar directory

--- sistema_escolar/includes/footer.php
+++
@@ -1,9 +0,0 @@
-    </main><!-- /page-content -->
-  </div><!-- /main-content -->
-</div><!-- /wrapper -->
-
-<div class="toast-container" id="toastContainer"></div>
-
-<script src="/sistema_escolar/assets/js/main.js"></script>
-</body>
-</html>
